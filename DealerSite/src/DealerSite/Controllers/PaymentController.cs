2f4f819ef56df39638c1f866ab3fce83ed9eabd5
RollerKostr <kmc1991@yandex.ru>
2018-01-09 15:18:02 +0300

    first try to display library contract

---
+++ DealerSite/src/DealerSite/Controllers/PaymentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CyberPlatGate.Contracts.Gate;
+
+namespace DealerSite.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        public ActionResult Index()
+        {
+            var gateCheckResult = new GateCheckResponse()
+            {
+                Error = null,
+                Session = "abcdef",
+                DisplayInfo = "Show this text to user",
+            };
+
+            return View(gateCheckResult);
+        }
+    }
+}
